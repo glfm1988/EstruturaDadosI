c4a32a9b4bc250614c74806b0166acb36be60ca1
gfari <gfari@172.17.6.31>
2016-09-29 19:53:02 -0300

    Sem ordenação.

--- EstruturaDadosI/src/seção8/Ex01.java
+++ EstruturaDadosI/src/seção8/Ex01.java
@@ -13,7 +13,7 @@ TESTE TESTE TESTE
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+//import java.util.Arrays;
 
 public class Ex01 {
 	public static void main(String[] args) {
@@ -31,7 +31,7 @@ public class Ex01 {
 				x = Integer.parseInt(leitor.readLine());
 				M[i] = x;
 			}
-			Arrays.sort(M);
+			//Arrays.sort(M);
 			ImprimeVetor(M);
 		} catch (NumberFormatException | IOException e) {
 		}
