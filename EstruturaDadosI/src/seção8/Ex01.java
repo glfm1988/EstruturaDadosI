47c026ab8a42a98523ce3999397d7b5e2eb25db2
gfari <gfari@172.17.6.31>
2016-09-29 20:06:53 -0300

    Alterei comentarios da classe

--- EstruturaDadosI/src/seção8/Ex01.java
+++ EstruturaDadosI/src/seção8/Ex01.java
@@ -6,8 +6,6 @@ janela enquanto o usu�rio n�o pressionar Cancel ou
 fechar a janela. Os n�meros dever�o ser armazenados
 em um array. Ao final, o programa dever� imprimir os
 n�meros em ordem crescente.
-
-TESTE TESTE TESTE
  */
 
 import java.io.BufferedReader;
